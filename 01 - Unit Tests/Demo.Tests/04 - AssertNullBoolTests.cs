c2f97c492b27ecdfca8d2ca9fe1f332e3a64aca2
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-27 19:02:46 -0300

    Testing some Nickname from employee possibilities

--- 01 - Unit Tests/Demo.Tests/04 - AssertNullBoolTests.cs
+++ 01 - Unit Tests/Demo.Tests/04 - AssertNullBoolTests.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Xunit;
 
 namespace Demo.Tests
 {
-    class AssertNullBoolTests
+    public class AssertNullBoolTests
     {
+        [Fact]
+        public void Employer_Name_CannotBeNullOrEmpty()
+        {
+            // Arrange and Act
+            var employee = new Employee("", 1000);
+
+            // Assert
+            Assert.False(string.IsNullOrEmpty(employee.Name));
+        }
+
+        [Fact]
+        public void Employee_Nickname_ShouldNotHaveNickname()
+        {
+            // Arrange and Act
+            var employee = new Employee("Thiago", 1000);
+
+            // Assert
+            Assert.Null(employee.Nickname);
+
+            // Assert Bool
+            Assert.True(string.IsNullOrEmpty(employee.Nickname));
+            Assert.False(employee.Nickname?.Length > 0);
+        }
     }
 }
