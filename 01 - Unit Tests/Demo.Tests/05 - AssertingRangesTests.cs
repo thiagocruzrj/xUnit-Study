970b930dfbf188e4c2c7c2612a510ca5e39c81bf
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-27 19:13:46 -0300

    Testing Salary level employee possibilities

--- 01 - Unit Tests/Demo.Tests/05 - AssertingRangesTests.cs
+++ 01 - Unit Tests/Demo.Tests/05 - AssertingRangesTests.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Xunit;
 
 namespace Demo.Tests
 {
-    class AssertingRangesTests
+    public class AssertingRangesTests
     {
+        [Theory]
+        [InlineData(700)]
+        [InlineData(1500)]
+        [InlineData(2000)]
+        [InlineData(7500)]
+        [InlineData(8000)]
+        [InlineData(15000)]
+        public void Employee_Salary_SalaryRangeMustRespectSalaryLevel(double salary)
+        {
+            // Arrange and Act
+            var employee = new Employee("Thiago", salary);
+
+            // Assert
+            if (employee.ProfessionalLevel == ProfessionalLevel.Junior)
+                Assert.InRange(employee.Salary, 500, 1999);
+
+            if (employee.ProfessionalLevel == ProfessionalLevel.Mid)
+                Assert.InRange(employee.Salary, 2000, 7999);
+
+            if (employee.ProfessionalLevel == ProfessionalLevel.Senior)
+                Assert.InRange(employee.Salary, 8000, double.MaxValue);
+
+            Assert.NotInRange(employee.Salary, 0, 499);
+        }
     }
 }
