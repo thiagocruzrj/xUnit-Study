8579f5ac7d06398a0dabfa870c1c9f65b9bcbb5c
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-27 17:36:27 -0300

    Implementing InlineData to create a lot of test with different values

--- 01 - Unit Tests/Demo.Tests/CalculatorTests.cs
+++ 01 - Unit Tests/Demo.Tests/CalculatorTests.cs
@@ -16,5 +16,24 @@ namespace Demo.Tests
             // Assert
             Assert.Equal(4, result);
         }
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,2,4)]
+        [InlineData(4,2,6)]
+        [InlineData(7,3,10)]
+        [InlineData(6,6,12)]
+        [InlineData(9,9,18)]
+        public void Calculator_Sum_ReturnCorrectSumValues(double v1, double v2, double total)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Sum(v1, v2);
+
+            // Assert
+            Assert.Equal(total, result);
+        }
     }
 }
