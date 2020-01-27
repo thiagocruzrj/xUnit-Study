ec851a9393ac8d87ec7201aeb1cc35c8bb23fd2f
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-27 17:29:51 -0300

    Testing first method with xUnit

---
+++ 01 - Unit Tests/Demo.Tests/CalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Calculator_Sum_ReturnSumValue()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Sum(2, 2);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+}
