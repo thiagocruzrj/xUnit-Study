25b6a42f3f3c772ce30e008515a1fe1f5ea21c63
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-27 18:55:39 -0300

    Testing some Calculator possibilities

--- 01 - Unit Tests/Demo.Tests/03 - AssertNumbersTests.cs
+++ 01 - Unit Tests/Demo.Tests/03 - AssertNumbersTests.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace Demo.Tests
 {
-    class AssertNumbersTests
+    public class AssertNumbersTests
     {
+        [Fact]
+        public void Calculator_Sum_MustBeEqual()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Sum(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Calculator_Sum_NotBeEqual()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Sum(1.33333, 1.77778);
+
+            // Assert
+            Assert.NotEqual(3.01, result, 5);
+        }
     }
 }
