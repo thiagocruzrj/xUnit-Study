4670520a5a36a6e16e188f1dd85b015a5271edc0
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-28 20:46:58 -0300

    Start ordering test implementation

---
+++ 01 - Unit Tests/Features/03 - Order/OrderTests.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+
+namespace Features.Tests._03___Order
+{
+    public class OrderTests
+    {
+        public static bool Test1Call;
+        public static bool Test2Call;
+        public static bool Test3Call;
+        public static bool Test4Call;
+
+
+        [Fact(DisplayName = "Test 1")]
+        [Trait("Category", "Tests Ordering")]
+        public void Test1()
+        {
+            Test1Call = true;
+
+            Assert.False(Test2Call);
+            Assert.False(Test3Call);
+            Assert.False(Test4Call);
+        }
+
+        [Fact(DisplayName = "Test 2")]
+        [Trait("Category", "Tests Ordering")]
+        public void Test2()
+        {
+            Test1Call = true;
+
+            Assert.False(Test1Call);
+            Assert.False(Test3Call);
+            Assert.False(Test4Call);
+        }
+
+        [Fact(DisplayName = "Test 3")]
+        [Trait("Category", "Tests Ordering")]
+        public void Test3()
+        {
+            Test1Call = true;
+
+            Assert.False(Test1Call);
+            Assert.False(Test2Call);
+            Assert.False(Test4Call);
+        }
+
+        [Fact(DisplayName = "Test 4")]
+        [Trait("Category", "Tests Ordering")]
+        public void Test4()
+        {
+            Test1Call = true;
+
+            Assert.False(Test1Call);
+            Assert.False(Test2Call);
+            Assert.False(Test3Call);
+        }
+    }
+}
