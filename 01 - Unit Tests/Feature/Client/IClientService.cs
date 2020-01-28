2a8be455eee0ca92a8d1858daa52f2f39931f20a
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-27 22:02:09 -0300

    Start implementation Client Service

--- 01 - Unit Tests/Feature/Client/IClientService.cs
+++ 01 - Unit Tests/Feature/Client/IClientService.cs
@@ -4,7 +4,12 @@ using System.Text;
 
 namespace Feature.Client
 {
-    public interface IClientService
+    public interface IClientService : IDisposable
     {
+        IEnumerable<Client> GetAllActives();
+        void Add(Client client);
+        void Update(Client client);
+        void Remove(Client client);
+        void Deactive(Client client);
     }
 }
