613cfa91ce5dabdbfd3a1f51c090c3f29fd35e61
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-28 18:03:59 -0300

    Implementing Client Service

--- 01 - Unit Tests/Feature/Client/ClientService.cs
+++ 01 - Unit Tests/Feature/Client/ClientService.cs
@@ -1,10 +1,63 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Feature.Client
 {
-    class ClientService
+    public class ClientService : IClientService
     {
+        private readonly IClientRepository _clienteRepository;
+        private readonly IMediator _mediator;
+
+        public ClientService(IClientRepository clienteRepository, IMediator mediator)
+        {
+            _clienteRepository = clienteRepository;
+            _mediator = mediator;
+        }
+
+        public IEnumerable<Client> GetAllActives()
+        {
+            return _clienteRepository.GetAll().Where(c => c.Active);
+        }
+
+        public void Add(Client client)
+        {
+            if (!client.IsValid())
+                return;
+
+            _clienteRepository.Add(client);
+            _mediator.Publish(new ClientEmailNotification("admin@me.com", client.Email, "Hi", "Welcome"));
+        }
+
+        public void Update(Client client)
+        {
+            if (!client.IsValid())
+                return;
+
+            _clienteRepository.Update(client);
+            _mediator.Publish(new ClientEmailNotification("admin@me.com", client.Email, "Changes", "Take a look"));
+        }
+
+        public void Remove(Client client)
+        {
+            _clienteRepository.Remove(client.Id);
+            _mediator.Publish(new ClientEmailNotification("admin@me.com", client.Email, "Bye", "Have a good trip"));
+        }
+
+        public void Deactive(Client client)
+        {
+            if (!client.IsValid())
+                return;
+
+            client.Deactive();
+            _clienteRepository.Update(client);
+            _mediator.Publish(new ClientEmailNotification("admin@me.com", client.Email, "Bye", "See you soon"));
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
