16d44cc7f1f591d884a8d65bdefb7ffd6e29c47c
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-29 18:48:29 -0300

    Starting testing with fluent assertions

--- 01 - Unit Tests/Features/07 - FluentAssertions/ClientFluentAssertionsTests.cs
+++ 01 - Unit Tests/Features/07 - FluentAssertions/ClientFluentAssertionsTests.cs
@@ -1,6 +1,47 @@
-﻿namespace Features.Tests._07___FluentAssertions
+﻿using Features.Tests._06___AutoMock;
+using Moq;
+using Xunit;
+
+namespace Features.Tests._07___FluentAssertions
 {
+    [Collection(nameof(ClientAutoMockerCollection))]
     public class ClientFluentAssertionsTests
     {
+        readonly ClientServiceAutoMockerFixture _clientServiceAutoMockerFixture;
+
+        public ClientFluentAssertionsTests(ClientServiceAutoMockerFixture clientServiceAutoMockerFixture)
+        {
+            _clientServiceAutoMockerFixture = clientServiceAutoMockerFixture;
+        }
+
+        [Fact(DisplayName = "New Valid Client")]
+        [Trait("Category", "Client Fluent Assertion Tests")]
+        public void Client_NewClient_ShouldBeValid()
+        {
+            // Arrage
+            var client = _clientServiceAutoMockerFixture.GenerateValidClient();
+
+            // Act
+            var result = client.IsValid();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(0, client.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "New Invalid Client")]
+        [Trait("Category", "Client Fluent Assertion Tests")]
+        public void Client_NewClient_ShouldBeInvalid()
+        {
+            // Arrage
+            var client = _clientServiceAutoMockerFixture.GenerateValidClient();
+
+            // Act
+            var result = client.IsValid();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEqual(0, client.ValidationResult.Errors.Count);
+        }
     }
 }
--- 01 - Unit Tests/Features/07 - FluentAssertions/ClientServiceFluentAssertionsTests.cs
+++ 01 - Unit Tests/Features/07 - FluentAssertions/ClientServiceFluentAssertionsTests.cs
@@ -1,6 +1,70 @@
-﻿namespace Features.Tests._07___FluentAssertions
+﻿using Feature.Client;
+using Features.Tests._06___AutoMock;
+using MediatR;
+using Moq;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Features.Tests._07___FluentAssertions
 {
+    [Collection(nameof(ClientAutoMockerCollection))]
     public class ClientServiceFluentAssertionsTests
     {
+        readonly ClientServiceAutoMockerFixture _clientTestAutoMockerFixture;
+        readonly ClientService _clientService;
+
+        public ClientServiceFluentAssertionsTests(ClientServiceAutoMockerFixture clientTestBogusFixture)
+        {
+            _clientTestAutoMockerFixture = clientTestBogusFixture;
+            _clientService = _clientTestAutoMockerFixture.GetClientService();
+        }
+
+        [Fact(DisplayName = "Add client with success")]
+        [Trait("Category", "Client Service Fluent Assertion Tests")]
+        public void ClientService_Add_ShouldExeculteWithSuccess()
+        {
+            // Arrage
+            var client = _clientTestAutoMockerFixture.GenerateValidClient();
+
+            // Act
+            _clientService.Add(client);
+
+            // Assert
+            _clientTestAutoMockerFixture.Mocker.GetMock<IClientRepository>().Verify(r => r.Add(client), Times.Once);
+            _clientTestAutoMockerFixture.Mocker.GetMock<IMediator>().Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
+        }
+
+        [Fact(DisplayName = "Add client with fail")]
+        [Trait("Category", "Client Service Fluent Assertion Tests")]
+        public void ClientService_Add_ShouldFailDueInvalidClient()
+        {
+            // Arrage
+            var client = _clientTestAutoMockerFixture.GenerateInvalidClient();
+
+            // Act
+            _clientService.Add(client);
+
+            // Assert
+            _clientTestAutoMockerFixture.Mocker.GetMock<IClientService>().Verify(r => r.Add(client), Times.Never);
+            _clientTestAutoMockerFixture.Mocker.GetMock<IMediator>().Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
+        }
+
+        [Fact(DisplayName = "Get active clients")]
+        [Trait("Category", "Client Service Fluent Assertion Tests")]
+        public void ClientService_GetAllActives_ShouldReturnOnlyActiveClients()
+        {
+            // Arrange
+
+            _clientTestAutoMockerFixture.Mocker.GetMock<IClientRepository>().Setup(c => c.GetAll())
+                .Returns(_clientTestAutoMockerFixture.GetSomeClients());
+
+            // Act
+            var clients = _clientService.GetAllActives();
+
+            // Assert
+            Assert.True(clients.Any());
+            Assert.False(clients.Count(c => !c.Active) > 0);
+        }
     }
 }
