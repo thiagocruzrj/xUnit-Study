42ce01ac3ec258897f133c4d31844dc199e59005
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-29 13:48:57 -0300

    Starting ClientService test with mock

--- 01 - Unit Tests/Features/05 - Mock/ClientServiceTest.cs
+++ 01 - Unit Tests/Features/05 - Mock/ClientServiceTest.cs
@@ -1,6 +1,42 @@
-﻿namespace Features.Tests._05___Mock
+﻿using Features.Tests._04___Human_Datas;
+using Xunit;
+
+namespace Features.Tests._05___Mock
 {
     public class ClientServiceTest
     {
+        readonly ClientTestBogusFixture _clientTestBogusFixture;
+
+        public ClientServiceTest(ClientTestBogusFixture clientTestBogusFixture)
+        {
+            _clientTestBogusFixture = clientTestBogusFixture;
+        }
+
+        [Fact(DisplayName = "Add client with success")]
+        [Trait("Category", "Client Service Mock Tests")]
+        public void ClientService_Add_ShouldExeculteWithSuccess()
+        {
+            // Arrage
+            // Act
+            // Assert
+        }
+
+        [Fact(DisplayName = "Add client with fail")]
+        [Trait("Category", "Client Service Mock Tests")]
+        public void ClientService_Add_ShouldFailDueInvalidClient()
+        {
+            // Arrage
+            // Act
+            // Assert
+        }
+
+        [Fact(DisplayName = "Get active clients")]
+        [Trait("Category", "Client Service Mock Tests")]
+        public void ClientService_GetAllActives_ShouldReturnOnlyActiveClients()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
     }
 }
